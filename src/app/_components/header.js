0f9e5509241640ae2c8243bfce94d5f11679912b
neycklyz <agarcreep@gmail.com>
2024-12-17 10:40:17 +0100

    init header

---
+++ src/app/_components/header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+      <header>
+        <nav className=" fixed top-0 left-0 right-0 text-center py-3">
+            <h1 className=" text-xl">La grande histoire du Mac</h1>
+        </nav>
+      </header>
+    );
+  };
+  
+  export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 // import Image from "next/image";
+import Header from './_components/header';
 import Hello from './_components/hello';
 
 export default function Home() {
   return (
     <main>
       <p>test</p>
+      <Header />
       <Hello />
     </main>
   );
