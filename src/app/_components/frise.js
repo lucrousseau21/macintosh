8896e6836ef63feaaab8909069a9fbb9ffc204a6
neycklyz <agarcreep@gmail.com>
2024-12-17 16:08:44 +0100

    hover frise

--- src/app/_components/frise.js
+++ src/app/_components/frise.js
@@ -1,10 +1,10 @@
 const Frise = () => {
     return (
       <div className=" fixed flex gap-4 h-screen right-0 top-1/2 bottom-0 vertical-rl z-40 px-4">
-        <a href="#">1980's</a>
-        <a href="#">1990's</a>
-        <a href="#">2000's</a>
-        <a href="#">2010's</a>
+        <a href="#" className="hover:bg-gray-200 py-2 rounded-full transition-all">1980's</a>
+        <a href="#" className="hover:bg-gray-200 py-2 rounded-full transition-all">1990's</a>
+        <a href="#" className="hover:bg-gray-200 py-2 rounded-full transition-all">2000's</a>
+        <a href="#" className="hover:bg-gray-200 py-2 rounded-full transition-all">2010's</a>
       </div>
     );
   };
