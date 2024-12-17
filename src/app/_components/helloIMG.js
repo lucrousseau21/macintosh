41fa10c43d0984de261449d715288432e8552d49
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-12-17 10:47:28 +0100

    helloimg

--- src/app/_components/helloIMG.js
+++ src/app/_components/helloIMG.js
@@ -1,7 +1,9 @@
-const Hello = () => {
-  return (
-    <div></div>
-  );
+const HelloIMG = () => {
+return (
+    <div className="flex justify-center items-center h-screen">
+        <img src="images/helloIMG.jpg" alt="HelloLogoJPG" />
+    </div>
+);
 };
 
-export default Hello;
+export default HelloIMG;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 import Header from './_components/header';
 import Hello from './_components/hello';
+import HelloIMG from './_components/helloIMG';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <p>test</p>
       <Header />
       <Hello />
+      <HelloIMG />
     </main>
   );
 }
