e66655bc3c747bed40fdf66733de25dbcc25b2c3
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-12-17 20:44:49 +0100

    footer

--- src/app/_components/Footer.js
+++ src/app/_components/Footer.js
@@ -1,10 +1,11 @@
 const Footer = () => {
-    return (
-      <div className="">
-        <p>@Copyrigth 2024 MMI @ IUT de Lens - All rights reserved</p>
+  return (
+    <footer className="bg-[#EDEEF1]">
+      <div className="max-w-4xl px-4 m-auto py-8">
+        <p className="text-sm">@Copyrigth 2024 - MMI - IUT de Lens - All rights reserved</p>
       </div>
-    );
-  };
-  
-  export default Footer;
-  
+    </footer>
+  );
+};
+
+export default Footer;
