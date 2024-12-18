d65dba4644466ac2ea67417a7ade82d1e2f93cb7
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-12-18 07:10:38 +0100

    header et favicon

--- src/app/_components/header.js
+++ src/app/_components/header.js
@@ -1,8 +1,11 @@
 const Header = () => {
     return (
       <header>
-        <nav className=" fixed top-0 left-0 right-0 text-center py-3 backdrop-blur-xl bg-background/60">
-            <h1 className=" text-xl">La grande histoire du Mac</h1>
+        <nav className=" fixed top-0 left-0 right-0 py-3 backdrop-blur-xl bg-background/60">
+        <div className="flex flex-row items-center">
+          <img src="images/logo.png" alt="Logo d'Apple" className="h-10"/>
+          <h1 className=" text-xl"> / La grande histoire du Mac</h1>
+        </div>
         </nav>
       </header>
     );
