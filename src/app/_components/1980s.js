e2495ba1b164966ce3e12d076ea130854870877d
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-12-18 11:24:18 +0100

    ajout video produit

--- src/app/_components/1980s.js
+++ src/app/_components/1980s.js
@@ -1,10 +1,50 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "tailwindcss/tailwind.css";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const Annee80 = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const coolVideo = videoRef.current;
+
+    if (coolVideo) {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: coolVideo,
+          start: "top-=100% top",
+          end: "bottom+=100% bottom",
+          scrub: true,
+          markers: true,
+          // pin: true,
+          onUpdate: (self) => {
+            coolVideo.currentTime = coolVideo.duration * self.progress;
+          },
+        },
+      });
+
+      const handleMetadataLoaded = () => {
+        tl.to(coolVideo, { currentTime: coolVideo.duration });
+      };
+
+      coolVideo.addEventListener("loadedmetadata", handleMetadataLoaded);
+
+      return () => {
+        coolVideo.removeEventListener("loadedmetadata", handleMetadataLoaded);
+        ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+      };
+    }
+  }, []);
   return (
     <div className="w-screen" id="1980">
       <div className="w-full h-screen flex">
         <div className=" h-screen flex flex-col justify-between p-16 w-1/2">
           <h1 className="text-3xl">1980's</h1>
-          <p>
+          <p font-just text-lg>
             Les années 1980 : L’ère de la révolution informatique Le Macintosh
             original est lancé le 24 janvier 1984. Avec une publicité légendaire
             diffusée lors du Super Bowl, Apple promettait de briser le monopole
@@ -20,8 +60,18 @@ const Annee80 = () => {
             fabrique Apple.<a className="blink">|</a>
           </p>
         </div>
-        <div className="w-1/2 h-screen bg-red-500 pt-14">
-          <p>test</p>
+        <div className="w-1/2 h-screen pt-14 overflow-hidden">
+          <video
+            ref={videoRef}
+            className="absolute h-full object-cover"
+            playsInline
+            webkit-playsinline="true"
+            preload="auto"
+            muted
+          >
+            <source src="/videos/Macintosh-128k.mp4" type="video/mp4" />
+            {/* <source src="/videos/Macintosh-128k.webm" type="video/webm" /> */}
+          </video>
         </div>
       </div>
     </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        just: ['JUST', 'sans-serif'], 
+        labora: ['Labora', 'sans-serif'], 
+      }
     },
   },
   plugins: [],
