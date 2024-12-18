2f1b5dd0e921498c4ee46850bfa70880249973bb
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-12-18 13:49:01 +0100

    z

--- src/app/_components/transition.js
+++ src/app/_components/transition.js
@@ -2,7 +2,7 @@ const Transition = () => {
   return (
     <div className="relative w-screen h-fit z-20" id="transition">
       <img className="relative w-full z-20" src="images/transition.png"></img>
-      <img className="absolute bottom-0 left-0 z-30" src="images/plumes.svg" />
+      <img className="absolute bottom-0 left-0 z-30 w-5 h-auto" src="images/plume.svg" />
     </div>
   );
 };
