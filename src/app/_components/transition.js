cc8ad5feff2e0d00ed59dc3c81b9601dcaa130a9
neycklyz <agarcreep@gmail.com>
2024-12-18 11:33:55 +0100

    transition image edit

---
+++ src/app/_components/transition.js
@@ -0,0 +1,10 @@
+const Transition = () => {
+    return (
+      <div className="w-screen h-fit" id="transition">
+        <img className="w-full" src="images/transition.png"></img>
+      </div>
+    );
+  };
+  
+  export default Transition;
+  
