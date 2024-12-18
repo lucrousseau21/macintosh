d8e677fe420a2fe665d37f58950dc4b56e8edd12
neycklyz <agarcreep@gmail.com>
2024-12-18 15:02:54 +0100

    header edit

--- src/app/_components/SecondHero.js
+++ src/app/_components/SecondHero.js
@@ -1,18 +1,19 @@
 const SecondHero = () => {
   return (
-    <div className="h-[70vh] flex flex-row max-w-6xl px-4 m-auto">
-      <div className="w-1/2 flex flex-col gap-4">
+    <div className="h-[70vh] flex flex-row max-w-6xl px-4 py-16 m-auto border-b-2">
+      <div className="w-1/2 flex flex-col gap-6">
         <h1 className="text-5xl">La grande histoire du Mac</h1>
-        <p>
+        <p className="text-lg"> 
           L'exposition virtuelle sur l'histoire du Mac d'Apple par
           décénnie.
         </p>
+        <a href="#1980" className="bg-blue-500 w-fit text-white text-lg px-5 py-2 rounded-full border-2 border-blue-500 hover:bg-white hover:text-blue-500">Découvrir</a>
       </div>
-      <div className="h-full w-1/2 flex items-center justify-center">
+      <div className="h-full w-1/2 flex items-center justify-center pb-8">
         <img
           src="images/steve_jobs_imac.png"
           alt="Steve Jobs et un iMac sur ses genoux"
-          className="w-2/3"
+          className="w-3/5"
         />
       </div>
     </div>
