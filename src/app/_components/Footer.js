3d76be85a7d8c98e28b57bfee8b3d38e647953d6
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-12-18 19:22:21 +0100

    ajout git footer

--- src/app/_components/Footer.js
+++ src/app/_components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#EDEEF1]">
       <div className="max-w-4xl px-4 m-auto py-8">
-        <p className="text-sm">@Copyrigth 2024 - MMI - IUT de Lens - All rights reserved</p>
+        <p className="text-sm">@Copyrigth 2024 - MMI - IUT de Lens -  <a className="text-blue-500" href="https://github.com/lucrousseau21/macintosh" target="_blank">github.com/lucrousseau21/macintosh</a></p>
       </div>
     </footer>
   );
