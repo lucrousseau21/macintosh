941827001cce2ae18ba66d839a1a06f625d79f40
Luc Rousseau <luc.rousseaupro@gmail.com>
2024-12-18 20:05:45 +0100

    correctif1.1

--- src/app/_components/SecondHero.js
+++ src/app/_components/SecondHero.js
@@ -5,7 +5,7 @@ const SecondHero = () => {
         <h1 className="text-5xl">La grande histoire du Mac</h1>
         <p className="text-lg"> 
           L'exposition virtuelle sur l'histoire du Mac d'Apple par
-          décénnie.
+          décennie.
         </p>
         <a href="#1980" className="bg-blue-500 w-fit text-white text-lg px-5 py-2 rounded-full border-2 border-blue-500 hover:bg-white hover:text-blue-500 hover:shadow-xl hover:shadow-blue-300">Découvrir</a>
       </div>
